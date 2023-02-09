b3bd5c6280e938a94ecbce14d4f0d92711a9ba9c
zenitba <112856019+zenitba@users.noreply.github.com>
2023-02-09 16:36:09 +0100

    test

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ const app = express()
 app.get('/', function (req, res) {
   res.render('index', data)
 })
+// Stel het poortnummer in waar express op gaat luisteren
+app.set('port', process.env.PORT || 8000)
 
+// Start express op, haal het ingestelde poortnummer op
 app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
-
-
 
 
 
